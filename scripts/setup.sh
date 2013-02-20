900c22ac24de54ab89cd2c0a2ebc3486bd86f5f5
Richard Bronosky <richard@bronosky.com>
2013-02-20 01:34:27 -0500

    No need to call "sh -c" if you are not putting sudo on the line

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -73,10 +73,7 @@ echo 'net.ipv4.ip_forward=1' >>  /etc/sysctl.conf
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-
-sh -c "iptables-save > /etc/iptables.ipv4.nat"
-
-
+iptables-save > /etc/iptables.ipv4.nat
 
 # Start the hostapd service
 service hostapd restart
